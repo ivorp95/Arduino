d424762361aefc230a94bef2dff2343798c4ff8e
ivorp95 <ivpan@proton.me>
2024-11-10 21:14:49 +0100

    new file:   sim900_stanjeRacuna/sim900_stanjeRacuna.ino

---
+++ sim900_stanjeRacuna/sim900_stanjeRacuna.ino
@@ -0,0 +1,14 @@
+#include <SoftwareSerial.h>
+#include <sim900.h>
+
+SoftwareSerial serial900(7,8);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
