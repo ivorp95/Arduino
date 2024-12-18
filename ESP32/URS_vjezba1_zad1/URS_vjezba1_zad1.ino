957e7538d73587f8e95bbd72454774d1892e7efd
ivorp95 <ivpan@proton.me>
2024-12-19 00:03:20 +0100

    modified:   ESP32/URS_vjezba1_zad1/URS_vjezba1_zad1.ino

--- ESP32/URS_vjezba1_zad1/URS_vjezba1_zad1.ino
+++ ESP32/URS_vjezba1_zad1/URS_vjezba1_zad1.ino
@@ -6,8 +6,8 @@ int Plava = 17;
 int internalLED=13;
 
 char *stringovi[]{"Crvena 1","Crvena 0", "crvena 1", "crvena 0", "CRVENA 1","CRVENA 0",
-"Zelena 1","Zelena 0","zelena 1","zelena 0","ZELENA 1","ZELENA 0",
-"Plava 1","Plava 0","plava 1","plava 0","PLAVA 1","PLAVA 0"};
+"Zelena 1","Zelena 0", "zelena 1", "zelena 0", "ZELENA 1","ZELENA 0",
+"Plava 1","Plava 0", "plava 1", "plava 0", "PLAVA 1","PLAVA 0"};
 
 String unos;
 
@@ -32,9 +32,6 @@ void loop()
 
   while(Serial.available()){
 
-    //digitalWrite(13, HIGH);
-    //Serial.println("testis 2");
-
     unos=Serial.readString();
     Serial.println(unos);
 
@@ -50,21 +47,21 @@ void loop()
     }   
 
 
-    else if(unos=="Zelena 1" || unos=="zelena 1"|| unos=="ZELENA 1"){
+    else if(unos==stringovi[6] || unos==stringovi[8]|| unos==stringovi[10]){
         digitalWrite(Zelena, HIGH);
         Serial.println("Zelena LED upaljena");
     }
-    else if(unos=="Zelena 0" || unos=="zelena 0"|| unos=="ZELENA 0"){
+    else if(unos==stringovi[7] || unos==stringovi[9]|| unos==stringovi[11]){
         digitalWrite(Zelena, LOW);
         Serial.println("Zelena LED ugasena");
     }
 
 
-    else if(unos=="Plava 1" || unos=="plava 1" || unos=="PLAVA 1"){
+    else if(unos==stringovi[12] || unos==stringovi[14] || unos==stringovi[16]){
         digitalWrite(Plava, HIGH);
         Serial.println("Plava LED upaljena");
     }
-    else if(unos=="Plava 0" || unos=="plava 0" || unos=="PLAVA 0"){
+    else if(unos==stringovi[13] || unos==stringovi[15] || unos==stringovi[17]){
         digitalWrite(Plava, LOW);
         Serial.println("Plava LED ugasena");
     }
