ff9b5a0fc07fe0f17f1c040e3e2e90b0c2e3a159
ivorp95 <ivpan@proton.me>
2025-01-16 13:45:30 +0100

    new file:   URS_vjezba3_I2C/URS_vjezba3_I2C.ino

---
+++ ESP32/URS_vjezba3_I2C/URS_vjezba3_I2C.ino
@@ -0,0 +1,89 @@
+#include "TCA9548A.h"
+
+#ifdef SOFTWAREWIRE
+  #include <SoftwareWIRE.h>
+  SoftwareWire myWIRE(3, 2);
+  TCA9548A<SoftwareWire> TCA;
+  #define WIRE myWIRE
+#else   
+  #include <Wire.h>
+  TCA9548A<TwoWire> TCA;
+  #define WIRE Wire
+#endif
+
+#define SERIAL Serial
+
+void setup(){
+  SERIAL.begin(115200);
+  while(!SERIAL){};
+
+  //WIRE.begin();
+  TCA.begin(WIRE);
+
+  TCA.openChannel(TCA_CHANNEL_0);   //TCA.closeChannel(TCA_CHANNEL_0);
+  TCA.openChannel(TCA_CHANNEL_1); //TCA.closeChannel(TCA_CHANNEL_1);
+  TCA.openChannel(TCA_CHANNEL_2); //TCA.closeChannel(TCA_CHANNEL_2);
+  TCA.openChannel(TCA_CHANNEL_3); //TCA.closeChannel(TCA_CHANNEL_3);
+  TCA.openChannel(TCA_CHANNEL_4); //TCA.closeChannel(TCA_CHANNEL_4);
+  TCA.openChannel(TCA_CHANNEL_5); //TCA.closeChannel(TCA_CHANNEL_5);
+  TCA.openChannel(TCA_CHANNEL_6); //TCA.closeChannel(TCA_CHANNEL_6);
+  TCA.openChannel(TCA_CHANNEL_7); //TCA.closeChannel(TCA_CHANNEL_7); 
+
+  scanI2Cbus(); //funkcija koja pronadje adrese, izvrsiti samo jednom
+
+}
+
+void loop(){
+
+}
+
+
+
+
+
+
+
+
+void scanI2Cbus(){
+
+  uint8_t error, i2cAddress, devCount, unCount;
+
+  SERIAL.println("Scanning...");
+  devCount = 0;
+  unCount = 0;
+  for(i2cAddress = 1; i2cAddress < 127; i2cAddress++ )
+  {
+    WIRE.beginTransmission(i2cAddress);
+    error = WIRE.endTransmission();
+
+    if (error == 0)
+    {
+      SERIAL.print("I2C device found at 0x");
+      if (i2cAddress<16) SERIAL.print("0");
+      SERIAL.println(i2cAddress,HEX);
+      devCount++;
+    }
+    else if (error==4)
+    {
+      SERIAL.print("Unknow error at 0x");
+      if (i2cAddress<16) SERIAL.print("0");
+      SERIAL.println(i2cAddress,HEX);
+      unCount++;
+    }    
+  }
+
+  if (devCount + unCount == 0)
+    SERIAL.println("No I2C devices found\n");
+  else {
+    SERIAL.print(devCount);
+    SERIAL.print(" device(s) found");
+    if (unCount > 0) {
+      SERIAL.print(", and unknown error in ");
+      SERIAL.print(unCount);
+      SERIAL.print(" address");
+    }
+    SERIAL.println();
+  }
+  SERIAL.println();
+  delay(1000); 
+}
